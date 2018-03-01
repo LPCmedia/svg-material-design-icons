f5af5bc531754f53438408ed68faa179d7a6f918
Patrick Boudreau <pboudreau@acquisio.com>
2018-02-28 22:11:56 -0500

    [chore] build from minor version

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,55 @@
-var gulp = require('gulp');
-var replace = require('gulp-replace');
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const merge = require('merge-stream');
+const _ = require('lodash');
+const svgSprite = require('gulp-svg-sprite');
 
-gulp.task('svg-sprites', function() {
-  return gulp.src([
-      'node_modules/material-design-icons/sprites/svg-sprite/*.svg',
-      '!node_modules/material-design-icons/sprites/svg-sprite/*-symbol.svg',
-  ])
-    .pipe(replace(/id=\"ic_([0-9a-zA-z_]+)_24px\"/g, function(pattern, replacement) {
-        var out = replacement.replace(/_/g,'-');
-        return 'id="' + out + '"';
-    }))
-    .pipe(gulp.dest('dist'));
-});
+const ICON_CATEGORIES = [
+    'action',
+    'alert',
+    'av',
+    'communication',
+    'content',
+    'device',
+    'editor',
+    'file',
+    'hardware',
+    'image',
+    'maps',
+    'navigation',
+    'notification',
+    'places',
+    'social',
+    'toggle',
+];
+
+gulp.task('svg-sprites', () =>
+    _(ICON_CATEGORIES)
+        .map((category) =>
+            gulp.src([`./node_modules/material-design-icons/${category}/svg/production/*_24px.svg`])
+                .pipe(svgSprite(getSvgSpriteConfig(category))))
+        .thru(merge)
+        .value()
+        .pipe(replace(/id=\"ic_([0-9a-zA-z_]+)_24px\"/g, function (pattern, replacement) {
+            var out = replacement.replace(/_/g, '-');
+            return 'id="' + out + '"';
+        }))
+        .pipe(gulp.dest('./dist')));
+
+function getSvgSpriteConfig(category) {
+    return {
+        shape: {
+            dimension: {
+                maxWidth: 24,
+                maxHeight: 24
+            },
+        },
+        mode: {
+            css: {
+                bust: false,
+                dest: './',
+                sprite: `./svg-sprite-${category}.svg`,
+            },
+        }
+    };
+}
